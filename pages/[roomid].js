f36800faf46405466e825a98c187dbc2e4aba4c7
akashdasp <akash.dasp@gmail.com>
2023-12-25 19:17:22 +0530

    setting up the stream

--- pages/[roomid].js
+++ pages/[roomid].js
@@ -1,10 +1,16 @@
 import { useEffect } from 'react'
 import { useSocket } from '@/context/socket'
 import usePeer from '@/hooks/usePeer'
+import useMediaStream from '@/hooks/useMediaStream'
+
 
 const Room = () => {
     const socket = useSocket();
     const { peer, myId } = usePeer();
+    const {stream}=useMediaStream()
+    return (<div>
+        Stream Succes
+    </div>)
 };
 
 export default Room
